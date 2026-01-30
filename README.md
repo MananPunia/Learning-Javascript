24c0effff59d2161b97e1d4b7b22192d534c0bfe
MananPunia <puniamanan432@gmail.com>
2026-01-30 12:25:19 +0530

    add Var file

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Learning-Javascript
 
+1.JS is dynamically typed is it automatically typecasts
+
+2.  fullname -> Not preffered (most preffered)
+    fullName -> Camel case
+    FullName -> 
+    full-name -> kebab case
+    full_name -> Snake Case
---
+++ Variables_and _stuff.js
@@ -0,0 +1,12 @@
+console.log("Manan");
+/* 
+Var -   Varible can be re declared and updated , a global scoped variale
+Let -   variable can cannot be redeclared but can be updated , block scoped (most prefferd.) 
+Const - variable can be re declared or cant be updated, block scoped  usecase: pi = 3.14
+    LET and CONST came in 2015 in Ecma Script(ES) 6👍
+*/
+
+// var ab = 3;
+// var ab = 92;
+// let a = 0;
+// let a = 9; err
